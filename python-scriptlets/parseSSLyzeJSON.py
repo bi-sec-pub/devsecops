afc957d8649cd4113186580b8cab2f4688b975df
bi-sec <57387021+bi-sec-pub@users.noreply.github.com>
2021-09-01 12:42:14 +0200

    Update parseSSLyzeJSON.py
    python3

--- python-scriptlets/parseSSLyzeJSON.py
+++ python-scriptlets/parseSSLyzeJSON.py
@@ -23,48 +23,48 @@ if os.path.isfile(filepath):
 
     for entry in jsonContent['server_scan_results']:
         for sub in entry['scan_commands_results']['ssl_2_0_cipher_suites']['accepted_cipher_suites']:
-                print "__SSL2.0__"
-                print sub['cipher_suite']['name']
-                print "ShowStopper: WEAK Protocol version"
+                print("__SSL2.0__")
+                print(sub['cipher_suite']['name'])
+                print("ShowStopper: WEAK Protocol version")
                 countShowStopper += 1
         for sub in entry['scan_commands_results']['ssl_3_0_cipher_suites']['accepted_cipher_suites']:
-                print "__SSL3.0__"
-                print sub['cipher_suite']['name']
-                print "ShowStopper: WEAK Protocol version"
+                print("__SSL3.0__")
+                print(sub['cipher_suite']['name'])
+                print("ShowStopper: WEAK Protocol version")
                 countShowStopper += 1
         for sub in entry['scan_commands_results']['tls_1_0_cipher_suites']['accepted_cipher_suites']:
-                print "__TLS1.0__"
-                print sub['cipher_suite']['name']
+                print("__TLS1.0__")
+                print(sub['cipher_suite']['name'])
                 if sub['cipher_suite']['name'].find('CBC') > 0:
-                        print "Warning: WEAK CBC-Cipher"
+                        print("Warning: WEAK CBC-Cipher")
                         countWarning += 1
         for sub in entry['scan_commands_results']['tls_1_1_cipher_suites']['accepted_cipher_suites']:
-                print "__TLS1.1__"
-                print sub['cipher_suite']['name']
+                print("__TLS1.1__")
+                print(sub['cipher_suite']['name'])
                 if sub['cipher_suite']['name'].find('CBC') > 0:
-                        print "Warning: WEAK CBC-Cipher"
+                        print("Warning: WEAK CBC-Cipher")
                         countWarning += 1
         for sub in entry['scan_commands_results']['tls_1_2_cipher_suites']['accepted_cipher_suites']:
-                print "__TLS1.2.__"
-                print sub['cipher_suite']['name']
+                print("__TLS1.2.__")
+                print(sub['cipher_suite']['name'])
                 if sub['cipher_suite']['name'].find('CBC') > 0:
-                        print "Warning: WEAK CBC-Cipher"
+                        print("Warning: WEAK CBC-Cipher")
                         countWarning += 1
         for sub in entry['scan_commands_results']['tls_1_3_cipher_suites']['accepted_cipher_suites']:
-                print "__TLS1.3__"
-                print sub['cipher_suite']['name']
+                print("__TLS1.3__")
+                print(sub['cipher_suite']['name'])
                 if sub['cipher_suite']['name'].find('CBC') > 0:
-                        print "Warning: WEAK CBC-Cipher"
+                        print("Warning: WEAK CBC-Cipher")
                         countWarning += 1
 
         pass
 
-print "_________________________"
-print "Summary: "
-print "Warnings: " + str(countWarning) + "/" + str(maxWarnings)
+print("_________________________")
+print("Summary: ")
+print("Warnings: " + str(countWarning) + "/" + str(maxWarnings))
 if countWarning > maxWarnings:
-    print "Recommendation: abort"
-print "Showstopper: " + str(countShowStopper) + "/" + str(maxShowStopper)
+    print("Recommendation: abort")
+print("Showstopper: " + str(countShowStopper) + "/" + str(maxShowStopper))
 if countShowStopper > maxShowStopper:
-    print "Recommendation: abort"
-print "_________________________"
+    print("Recommendation: abort")
+print("_________________________")
